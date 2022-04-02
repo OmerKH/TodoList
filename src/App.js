4378fac97ce0ee87f2b0fb6657a8f46371537ad2
OMER KH <omer.khs@gmail.com>
2022-04-02 17:19:04 +0300

    Fine-Tuning

--- src/App.js
+++ src/App.js
@@ -1,68 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import Form from "./components/Form";
-import ToDoList from "./components/TodoList";
 
 function App() {
-  // State
-  const [input, setInput] = useState("");
-  const [toDo, setToDo] = useState([]);
-  const [status, setStatus] = useState("All");
-  const [filtered, setFiltered] = useState([]);
-
-  //Effect-1
-  useEffect(() => {
-    localGet();
-  }, []);
-
-  //Effect-2
-  useEffect(() => {
-    localStore();
-    handleSwitch();
-  }, [toDo, status]);
-
-  //Storage
-  const localStore = () => {
-    localStorage.setItem("toDo", JSON.stringify(toDo));
-  };
-
-  const localGet = () => {
-    if (localStorage.getItem("toDo") === null) {
-      localStorage.setItem("toDo", JSON.stringify([]));
-    } else {
-      let storage = JSON.parse(localStorage.getItem("toDo"));
-      setToDo(storage);
-    }
-  };
-
-  //Function
-  const handleSwitch = () => {
-    switch (status) {
-      case "completed":
-        setFiltered(toDo.filter((task) => task.completed === true));
-        break;
-      case "uncompleted":
-        setFiltered(toDo.filter((task) => task.completed === false));
-        break;
-      default:
-        setFiltered(toDo);
-        break;
-    }
-  };
-
   return (
     <div className="App">
       <header>
         <h1>Task List</h1>
       </header>
-      <Form
-        setInput={setInput}
-        input={input}
-        toDo={toDo}
-        setToDo={setToDo}
-        setStatus={setStatus}
-      />
-      <ToDoList toDo={toDo} setToDo={setToDo} filtered={filtered} />
+      <Form />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ToDoList from "./TodoList";
 
-const Form = ({ setInput, input, toDo, setToDo, setStatus }) => {
+const Form = () => {
+  // State
+  const [input, setInput] = useState("");
+  const [toDo, setToDo] = useState([]);
+  const [status, setStatus] = useState("All");
+  const [filtered, setFiltered] = useState([]);
+
+  //Effect-1
+  useEffect(() => {
+    localGet();
+  }, []);
+
+  //Effect-2
+  useEffect(() => {
+    localStore();
+    handleSwitch();
+  }, [toDo, status]);
+
+  //Storage
+  const localStore = () => {
+    localStorage.setItem("toDo", JSON.stringify(toDo));
+  };
+
+  const localGet = () => {
+    if (localStorage.getItem("toDo") === null) {
+      localStorage.setItem("toDo", JSON.stringify([]));
+    } else {
+      let storage = JSON.parse(localStorage.getItem("toDo"));
+      setToDo(storage);
+    }
+  };
+
+  //Function
+  const handleSwitch = () => {
+    switch (status) {
+      case "completed":
+        setFiltered(toDo.filter((task) => task.completed === true));
+        break;
+      case "uncompleted":
+        setFiltered(toDo.filter((task) => task.completed === false));
+        break;
+      default:
+        setFiltered(toDo);
+        break;
+    }
+  };
+
   const textHandler = (e) => {
     setInput(e.target.value);
   };
@@ -18,25 +65,28 @@ const Form = ({ setInput, input, toDo, setToDo, setStatus }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="select">
-        <select onChange={handleStatus} name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-      <input
-        onChange={textHandler}
-        type="text"
-        className="todo-input"
-        placeholder="Add Task"
-        value={input}
-      />
-      <button className="todo-button" type="submit">
-        Submit
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="select">
+          <select onChange={handleStatus} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+        <input
+          onChange={textHandler}
+          type="text"
+          className="todo-input"
+          placeholder="Add Task"
+          value={input}
+        />
+        <button className="todo-button" type="submit">
+          Submit
+        </button>
+      </form>
+      <ToDoList toDo={toDo} setToDo={setToDo} filtered={filtered} />
+    </div>
   );
 };
 export default Form;
